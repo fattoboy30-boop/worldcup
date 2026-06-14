c0306d25432a21bcf8fad50b1c93a2a6fc786e0f
Fattoboy30 <fattoboy30@gmail.com>
2026-06-15 06:05:44 +1100

    Add fullscreen button for Arena Sport 1 Premium stream

--- script.js
+++ script.js
@@ -287,6 +287,35 @@ function initStreaming() {
             }
         })
         .catch(() => {});
+
+    const fullscreenBtn = document.getElementById('fullscreenBtn');
+    const embedContainer = document.querySelector('.live-stream-embed');
+
+    if (fullscreenBtn && embedContainer) {
+        fullscreenBtn.addEventListener('click', () => {
+            if (!document.fullscreenElement && !document.webkitFullscreenElement && !document.mozFullScreenElement) {
+                if (embedContainer.requestFullscreen) {
+                    embedContainer.requestFullscreen();
+                } else if (embedContainer.webkitRequestFullscreen) {
+                    embedContainer.webkitRequestFullscreen();
+                } else if (embedContainer.mozRequestFullScreen) {
+                    embedContainer.mozRequestFullScreen();
+                } else if (embedContainer.msRequestFullscreen) {
+                    embedContainer.msRequestFullscreen();
+                }
+            } else {
+                if (document.exitFullscreen) {
+                    document.exitFullscreen();
+                } else if (document.webkitExitFullscreen) {
+                    document.webkitExitFullscreen();
+                } else if (document.mozCancelFullScreen) {
+                    document.mozCancelFullScreen();
+                } else if (document.msExitFullscreen) {
+                    document.msExitFullscreen();
+                }
+            }
+        });
+    }
 }
 
 // ===== Particles =====
